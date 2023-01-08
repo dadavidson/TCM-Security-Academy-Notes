a0bf940c94d9aefe290f9cd92902387f943d8273
dadavidson <r3dd3ath@gmail.com>
2023-01-08 03:47:22 -0500

    Commit variables-demo.py

---
+++ Python-101-for-Hackers/1. Python 101/variables-demo.py
@@ -0,0 +1,51 @@
+name = "neut"
+print(name)
+
+name_length = 4
+print(name_length)
+
+name, name_length = "neut", 4
+
+print(type(name))
+print(type(name_length))
+
+name_length = int("4")
+print(type(name_length))
+
+# name = int('neut')
+
+name_length = 4
+Name_length = 5
+
+print(name_length)
+print(Name_length)
+
+name_list = ["neut", "247CTF", "asd"]
+print(type(name_list))
+
+name1, name2, name3 = name_list
+print(name1)
+print(name2)
+print(name3)
+
+name_tuple = ("neut", "247CTF")
+print(type(name_tuple))
+
+name_dictionary = {"neut": 4, "247CTF": 6}
+print(type(name_dictionary))
+
+name_boolean = True
+print(type(name_boolean))
+
+name_range = range(6)
+print(type(name_range))
+
+name_bytes = b"neut2"
+print(type(name_bytes))
+
+print(name_tuple)
+print(name_list)
+print(name_dictionary)
+print(name_boolean)
+print(name_range)
+print(name_bytes)
