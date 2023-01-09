bb170fc3b261cc42eaf9971703bafda7d3ac2624
dadavidson <r3dd3ath@gmail.com>
2023-01-09 14:13:02 -0500

    Commit numbers-demon.py

---
+++ Python-101-for-Hackers/1. Python 101/numbers-demo.py
@@ -0,0 +1,33 @@
+t1_int = 1
+t1_float = 1.0
+
+print(t1_int)
+print(t1_float)
+
+print(type(t1_int))
+print(type(t1_float))
+
+t1_complex = 3.14j
+
+print(t1_complex)
+print(type(t1_complex))
+
+t1_hex = 0xa
+print(t1_hex)
+print(type(t1_hex))
+
+t1_octal = 0o10
+print(t1_octal)
+print(type(t1_octal))
+
+print(1 + 0x1 + 0o1)
+
+print(abs(4))
+print(abs(-4))
+
+print(round(8.4))
+print(round(8.5))
+print(round(8.6))
+
+print(bin(8))
+print(hex(8))
